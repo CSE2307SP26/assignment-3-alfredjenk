74673cf3467585f0c07f31d5d90b4cd01aefbcac
alfredjenk <a.j.jenkins@wustl.edu>
2026-02-13 18:55:16 -0600

    after_grade

--- grading.sh
+++ grading.sh
@@ -2,24 +2,33 @@
 expected_out=$1
 student_out=$2
 
-
-
 while read student ; do
-    echo $student 
+    #echo $student 
 
-    git clone https://github.com/CSE2307SP26/$student.git
+    git -q clone https://github.com/CSE2307SP26/$student.git
     cd $student
-    git checkout cipher
-   
+    git -q checkout cipher
 
+    git checkout -q `git rev-list -n 1 --first-parent --before="2026-02-12 10:00" cipher`
 
-    git checkout `git rev-list -n 1 --first-parent --before="2026-02-12 10:00" cipher`
-
-    if [[ -f "Cipher.java" ]] #skeloton for chekcking cipherre
+    javac Cipher.java
+    java Cipher
+    
+    if [[ -f "$student_out" ]]
+    then
+        OUTPUT=$(cat $student_out)
+        RIGHT_OUTPUT=$(cat ../$expected_out)
+        if [[ $OUTPUT==$RIGHT_OUTPUT ]]
         then
-            echo "1"
+        echo "$student: 1"
         else
-            echo "0"
+        echo "$student: 0"
         fi
+    else
+        echo "$student: 0"
+    fi
+
+    cd ..
+    rm -rf "$student"
 
 done
